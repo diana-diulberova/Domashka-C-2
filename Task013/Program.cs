e502ddb442c97ea191dd8eb36de0764fef3b050d
Diana <d.dyulberova@gmail.com>
2022-09-06 18:05:32 +0500

    показывает третью цифру трехзначного числа

---
+++ Task013/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 99 ) 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+if (number > 999) 
+{
+    Console.WriteLine("Введено неправильное число");
+}
+
+else 
+{
+    int number2 = number%10;
+Console.WriteLine(number2);
+}
