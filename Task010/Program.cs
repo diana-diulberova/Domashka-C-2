bca1825d3c39b178908f1de8c9d3f5c9040195c6
Diana <d.dyulberova@gmail.com>
2022-09-06 17:57:50 +0500

    показывает вторую цифру в трехзначном числе

---
+++ Task010/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 99 || number > 999) 
+{
+    Console.WriteLine("Введено неправильное число");
+}
+else 
+{
+    int number2 = number / 10 % 10;
+Console.WriteLine(number2);
+}
