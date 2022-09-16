c4b46007226b55f840301d3081d1e5b54b8b00c9
Diana <d.dyulberova@gmail.com>
2022-09-16 19:44:13 +0500

    Исправила Task013 на нахождение третьей цифры любого числа

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -5,21 +5,39 @@
 32679 -> 6
 */
 
-Console.WriteLine("Введите трехзначное число: ");
+Console.WriteLine("Введите число: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
 if (number <= 99 ) 
 {
     Console.WriteLine("Третьей цифры нет");
 }
-
-if (number > 999) 
+else
 {
-    Console.WriteLine("Введено неправильное число");
-}
+    int devision = number / 100;
+    int index = 0;
+    while (devision > 1)
+    {
+        index++;
+        devision = devision / 10;
+    }
+    int thirdDigital = 0;
+    if(index > 1)
+    {
+        int degree = 10;
+        int i = 1;
+        while(i < index)
+        {
+            thirdDigital = number / degree % 10;
+            degree = degree * 10;
+            i++;
+        }
+    }
+    else
+    {
+        thirdDigital = number % 10;
+    }
 
-else 
-{
-    int number2 = number%10;
-Console.WriteLine(number2);
+    Console.WriteLine($"Третьей цифрой числа {number} является {thirdDigital}");
+
 }
